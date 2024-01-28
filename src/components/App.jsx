891a162fe08502cf08c36aa4a5871302a4fec1a4
Melodica4585 <melodica4585@gmail.com>
2024-01-28 12:28:13 +0200

    added code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,16 @@
-function App() {
-  return (App)
-}
+import { Profile } from './Profile/Profile.jsx';
+import { FriendList } from './FriendList/FriendList.jsx';
+import userData from '../userData.json';
+import friendsData from '../friends.json';
+import transactionsData from '../transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory.jsx';
 
-export default App
+export const App = () => {
+  return (
+    <div className="container">
+      <Profile data={userData} />
+      <FriendList friends={friendsData} />
+      <TransactionHistory transactions={transactionsData} />
+    </div>
+  );
+};
